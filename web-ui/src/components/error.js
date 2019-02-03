b587bfa179211a97300d59b202085964045158bb
Akash Sheth <absheth@iu.edu>
2019-02-03 01:18:26 +0000

    Fetch post and create post with fake api

---
+++ web-ui/src/components/error.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Error = () => {
+    return (
+        <div>
+            <p>Error: The page you are trying to access does not exist. </p>
+        </div>
+    )
+}
+
+export default Error
