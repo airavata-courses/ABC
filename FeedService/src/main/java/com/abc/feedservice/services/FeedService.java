ea77c58417b621cbf2e47ca4e6d1179cb8946df9
chetan253 <cpatil0@outlook.com>
2019-02-06 05:07:02 -0500

    fix

--- FeedService/src/main/java/com/abc/feedservice/services/FeedService.java
+++ FeedService/src/main/java/com/abc/feedservice/services/FeedService.java
@@ -3,14 +3,13 @@ package com.abc.feedservice.services;
 import com.abc.feedservice.model.Tweet;
 import com.abc.feedservice.repositories.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
  * @author chetan253 on 2/5/19
- * @apiNote
+ * @apiNote Service to get feed for user
  */
 
 @Component
