f86eb6f7355880cc3b8f1146de7e4ca0e9d41915
Bhushan Malgaonkar <bmalgaon@iu.edu>
2019-03-01 13:01:59 -0500

    added like functionality

--- web-ui/src/Feed/FeedPost.js
+++ web-ui/src/Feed/FeedPost.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 
-export function FeedPost (props) {
-	console.log(props)
-    return(
-        <div className="card border-dark mb-3">
-            <div className="card-header PostUserInfo">
-                <img src="https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg" alt="User Placeholder" className ="img-thumbnail rounded-circle" width="50" height="50"/>
-                <h5 style={{ padding: 10 }}>{/* props.data.firstName} {props.data.lastName*/ } Username - {props.data.userName}</h5>
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { LikeButton } from './LikeButton';
+
+
+class FeedPost extends Component {
+
+    render() {
+        console.log('Feedpost props: ', this.props);
+        return (
+            <div className="card border-dark mb-3">
+                <div className="card-header PostUserInfo">
+                    <img src="https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg" alt="User Placeholder" className="img-thumbnail rounded-circle" width="50" height="50" />
+                    <h5 style={{ padding: 10 }}>{/* props.data.firstName} {props.data.lastName*/} Username - {this.props.data.userName}</h5>
+                </div>
+
+                <div className="card-body">
+                    <p className="card-text text-dark"> {this.props.data.tweetText}</p>
+                    <LikeButton
+                        index={this.props.index}
+                        disabled={false}
+                        likeCount={this.props.data.likeCount}
+                        tweetId={this.props.data.id}
+                        userId={this.props.user.id}
+                    />
+                </div>
             </div>
-
-            <div className="card-body">
-                <p className="card-text text-dark"> {props.data.tweetText}</p>
-
-                <button className="btn btn-primary"
-                    onClick={props.incrementLike(props.data.id, props.user.username)}
-                    disabled={true}>
-                    <span className="badge badge-light">
-                        {/*props.data.id} {props.user.username*/} (disabled for now) Like
-                    </span> 
-
-                </button>
-                {/* parseInt(props.data.userId) === props.user.id
-                        && <button className="btn btn-danger"
-                            onClick={() => {props.onClickDelete(props.data.id)}}
-                        >
-                            Delete
-                        </button> */
-                }
-
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
+
+// Type checking
+FeedPost.propTypes = {
+
+};
+
+// redux updates the states, map states and props here
+const mapStateToProps = state => ({
+
+});
+
+
+const connectedFeed = connect(mapStateToProps, {})(FeedPost);
+export { connectedFeed as FeedPost };
