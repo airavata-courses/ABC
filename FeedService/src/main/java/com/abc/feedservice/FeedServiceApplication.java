9324aeda053be583147a29c9bccad819230d55cb
chetan253 <cpatil0@outlook.com>
2019-03-02 10:36:37 -0500

    Added classes to support tomcat deployment

--- FeedService/src/main/java/com/abc/feedservice/FeedServiceApplication.java
+++ FeedService/src/main/java/com/abc/feedservice/FeedServiceApplication.java
@@ -2,10 +2,16 @@ package com.abc.feedservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FeedServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FeedServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FeedServiceApplication.class, args);
