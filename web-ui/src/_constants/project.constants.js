4a902afa57405a7968ff7ce6e75780ab29e4c6c0
Akash B. Sheth <akashbsheth@gmail.com>
2019-03-03 14:14:15 +0000

    changed hosts to api gateway hosts

--- web-ui/src/_constants/project.constants.js
+++ web-ui/src/_constants/project.constants.js
@@ -1,4 +1,4 @@
 // export const URL = 'http://absheth-dev.grnoc.iu.edu:3001'
-export const USER_URL = 'http://localhost:3000'
-export const FEED_URL = 'http://localhost:8080'
-export const NEWS_URL = 'http://localhost:5000'
+export const USER_URL = 'http://149.165.169.38:8081/users'
+export const FEED_URL = 'http://149.165.169.38:8081/feed'
+export const NEWS_URL = 'http://149.165.169.38:8081/news'
