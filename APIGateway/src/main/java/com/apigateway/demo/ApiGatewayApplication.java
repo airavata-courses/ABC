fa64acf1237394887542d2666fed51d617a7780f
chetan253 <cpatil0@outlook.com>
2019-03-03 08:21:00 -0500

    Modified cors

--- APIGateway/src/main/java/com/apigateway/demo/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/apigateway/demo/ApiGatewayApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@CrossOrigin(origins = "*")
+@RestController
 public class ApiGatewayApplication {
 
     @Bean
