0101697d404ed2a9de55d17c6f594d444df8eb4a
Akash B. Sheth <akashbsheth@gmail.com>
2019-03-03 04:12:04 +0000

    Updated publish script

--- publish-news-service.sh
+++ publish-news-service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo pip install --upgrade pip
+# sudo pip install --upgrade pip
+sudo python -m pip uninstall pip && sudo apt install python-pip --reinstall
 sudo pip install -r requirement.txt
 python news-application.py >/dev/null 2>&1 &
