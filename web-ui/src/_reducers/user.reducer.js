103ca818ca74513e7dd8fc49d3f91fe10b2f0c1c
Akash B. Sheth <akashbsheth@gmail.com>
2019-03-04 03:58:21 +0000

    Fix for #98

--- web-ui/src/_reducers/user.reducer.js
+++ web-ui/src/_reducers/user.reducer.js
@@ -44,12 +44,12 @@ export default function users(state = {}, action) {
                     return user;
                 })
             };
-	//  case userConstants.LOGOUT:
-        //      // remove deleted user from state
-        //      return {
-	//  	...state,
-        //         	undefinded
-        //      };
+	case userConstants.LOGOUT:
+            // remove deleted user from state
+            return {
+		...state,
+		{}
+            };
         default:
             return state
     }
