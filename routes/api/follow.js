c753ed9328f189fd032c56759fd8ec1d4a82b638
chetan253 <cpatil0@outlook.com>
2019-03-07 23:33:06 -0500

    added following field in followers/following routes

--- routes/api/follow.js
+++ routes/api/follow.js
@@ -47,9 +47,7 @@ module.exports = (sequelize) => {
         if (!userId) return res.status(400).send();
         sequelize
             .query(
-                'SELECT follows.follower as userId, users.userName, users.firstName, users.lastName FROM follows, users WHERE follows.follower = users.userId AND follows.follower in (SELECT follower as follower FROM follows WHERE following="' +
-                userId +
-                '")'
+                'select u.userId, u.userName, u.firstName, u.lastName, u.email, g.following from users as u, (select f.follower, if(f.ifFollows IS NULL, "false", "true") as following from ( select f1.follower, f2.ifFollows from ( select follower from follows as f where f.following = "' + userId + '" ) as f1 left join ( select following as follower,TRUE as ifFollows from follows as f where f.follower = "' + userId + '") f2 on f1.follower = f2.follower) as f) as g where u.userId = g.follower;'
             )
             .then(result => {
                 console.log(result);
@@ -69,16 +67,17 @@ module.exports = (sequelize) => {
 
         sequelize
             .query(
-                'SELECT follows.following as userId, users.userName, users.firstName, users.lastName FROM follows, users WHERE follows.following = users.userId AND follows.following in (SELECT following FROM follows WHERE follower="' +
-                userId +
-                '")'
+                'select userId, userName, firstName, lastName, email from users where userId in (select following from follows where follower="' + userId + '")'
             )
             .then(result => {
-                console.log(result);
+                result = result[0];
+                for (let i = 0; i < result.length; ++i)
+                    result[i]['following'] = "true";
+
                 res
                     .set("Authorization", "Bearer fake-jwt-token")
                     .status(200)
-                    .send(result[0]);
+                    .send(result);
             })
             .catch(error => {
                 res.status(500).send({ error });
