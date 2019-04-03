600fb85700cd29759dde0ca32327a1684e123132
chetan253 <cpatil0@outlook.com>
2019-04-03 04:06:55 -0400

    Changed constants for kubernetes

--- web-ui/src/_constants/project.constants.js
+++ web-ui/src/_constants/project.constants.js
@@ -1,4 +1,4 @@
 // export const URL = 'http://absheth-dev.grnoc.iu.edu:3001'
-export const USER_URL = 'http://149.165.169.38:8081/users'
-export const FEED_URL = 'http://149.165.169.38:8081/feed'
-export const NEWS_URL = 'http://149.165.169.38:8081/news'
+export const USER_URL = 'http://149.165.169.214:30007/users'
+export const FEED_URL = 'http://149.165.169.214:30007/feed'
+export const NEWS_URL = 'http://149.165.169.214:30007/news'
