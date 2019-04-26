95d8f8a6bb5b5e977b7f4ce9fdfb6dcb2655b531
chetan253 <cpatil0@outlook.com>
2019-04-26 05:15:01 -0400

    Updated urls

--- FeedService/src/main/java/com/abc/feedservice/controller/FeedController.java
+++ FeedService/src/main/java/com/abc/feedservice/controller/FeedController.java
@@ -42,7 +42,7 @@ public class FeedController {
         headers.add("Authorization","Bearer fake-jwt-token");
         if(userId != null){
             RestTemplate restTemplate = new RestTemplate();
-            String baseURL = "http://localhost:30010/relation/user/"+ userId +"/following";
+            String baseURL = "http://user-mgmt-service:3001/relation/user/"+ userId +"/following";
             ResponseEntity<String> result = restTemplate.getForEntity(baseURL, String.class);
 
             JSONArray arr = new JSONArray(result.getBody());
